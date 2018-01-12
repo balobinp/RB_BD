7fdb5b1175479c0fc04cdc837b294346a041a006
Pavel Balobin <pavel@roamability.com>
2018-01-12 22:19:43 +0300

    180112

---
+++ pygraylog_yuri/endpoints.py
@@ -0,0 +1,6 @@
+endpoints = {
+    '/api/search/universal/absolute/terms': ['field', 'query', 'from_', 'to'],
+    '/api/search/universal/absolute' : ['query', 'from_', 'to'],
+    '/api/search/universal/relative/terms' : ['field', 'query', 'range'],
+    '/api/search/universal/relative' : [ 'query', 'range']
+}
