44b7ea8e9858e76a1a7adfcc26a87120d83d3490
Pavel Balobin <pavel@roamability.com>
2019-01-23 09:41:06 +0300

    190123

--- roamability.py
+++ roamability.py
@@ -2,16 +2,91 @@ import http
 import re
 from bs4 import BeautifulSoup
 import sys
+import os
+from os.path import isfile, isdir, join, normpath
+
+# SendMsu variables
 
 ss7_url = "172.18.11.10"
 ss7_path = "/cgi-bin/ss7gw.fcgi"
 get_info = "24027000001F0D00010000BFFF01000000000000102F978372C730834EF445172BD26B8C8F"
 get_info_mo = "24027000001F0D00210000BFFF01000000000000102F978372C730834EF445172BD26B8C8F"
 
+# Test function
 
 def test_func():
+    """Test function to say Greetings from Roamability!"""
     print('Greetings from Roamability!')
     return None
+
+
+# Search in files by Tags functions
+
+def find_files_by_tags(paths, all_tags, any_tags):
+    """
+    This function search given tags in given folders and print out the description.
+    paths parameter - Список файлов, в которых нужно искать файлы с расширением .txt
+    any_tags parameter - Если хотя бы один из тэгов присутствует. Нужно указать минимум один.
+    all_tags parameter - Если все теги присутствуют. Нужно оставить лист пустым [] если нет обязательных тэгов.
+    """
+    for path in paths:
+        files, folders = find_txt_files_and_folders(path)
+        search_tags_in_file(path, files, all_tags, any_tags)
+
+        for folder in folders:
+            folder_path = join(path, folder)
+            files, _ = find_txt_files_and_folders(folder_path)
+            search_tags_in_file(folder_path, files, all_tags, any_tags)
+
+def find_txt_files_and_folders(path_name):
+    file_names_all = os.listdir(path_name)
+    files = [file for file in file_names_all if isfile(join(path_name, file)) and file.find('.txt') != -1]
+    folders = [folder for folder in file_names_all if isdir(join(path_name, folder))]
+    return files, folders
+
+
+def search_tags_in_file(path_name, files_list, all_tags, any_tags):
+    for file in files_list:
+        with open(join(path_name, file)) as inf:
+            # Преобразовать файл в список. В качествер разделителя пробел. read читает весь файл.
+            words_in_file = inf.read().lower().split()
+        if any(item in words_in_file for item in any_tags):
+            if len(all_tags) > 0 and all(item in words_in_file for item in all_tags):
+                print_description_from_file(path_name, file)
+            elif len(all_tags) == 0:
+                print_description_from_file(path_name, file)
+    return None
+
+
+def print_description_from_file(path_name,file_name):
+    print(100*'*')
+    print(normpath(join(path_name, file_name)))
+    print(100*'-')
+    with open(join(path_name, file_name)) as inf:
+        # Построчно читаем файл
+        # Процедура неотимальна, т.к. будет прочитан весь файл до конца
+        for line in inf:
+            line.strip()
+            if '<DESCRIPTION>' in line:
+                for line in inf:
+                    line.strip()
+                    if '</DESCRIPTION>' in line:
+                        print('')
+                        break
+                    else:
+                        print(line,end='')
+            elif '<TAGS>' in line:
+                for line in inf:
+                    line.strip()
+                    if '</TAGS>' in line:
+                        print(30*'-')
+                        break
+                    else:
+                        print(line,end='')
+    return None
+
+
+# SendMsu functions
 
 def print_imsi_prof(ogt, imsi, msc, payload_type):
     if payload_type == 'as_resp':
